58fc819279497e3ce03094a43f4211c425e2f8ae
BrandenAdam <branden28adam@gmail.com>
2021-10-23 13:10:58 +0700

    commit

--- NG/exercise.py
+++ NG/exercise.py
@@ -5,19 +5,19 @@ Created on Tue Oct 19 14:09:40 2021
 @author: Shinsaragi
 """
 
-import random
+import random as rand
+import numpy as np
 
-rand = random.randint(1, 10)
-flag = "y" 
-count = 0
-while flag == "y":
-    if count >= 3: print("Game Over"); break
-    count += 1
-    inputs = int(input("Choose a number between 1 to 10: "))
-    if inputs < rand: print("Guessed number is too small")
-    elif inputs > rand: print("Guessed number is too big")
-    else: print("Congratz"); count = 0; flag = input("Want to play again? (y/n)") 
-    
+
+# array = np.random.randint(0, 3, size = (3,5,4))
+# print(array.T.shape)
+# print(array.sum(axis = 2))
+
+speed = [32,111,138,28,59,77,97]
+
+x = np.std(speed)
+
+print(x)
     
     
     
